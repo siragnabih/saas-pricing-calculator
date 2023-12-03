d483c40aa47c308c52ca686127472e3dfd30bbec
Sirag <siragnabih@gmail.com>
2023-12-03 23:06:38 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-// Function to update slider values
-function updateSliderValue(sliderId, valueId, isPercentage = false) {
-    const slider = document.getElementById(sliderId);
-    const valueDisplay = document.getElementById(valueId);
-    valueDisplay.textContent = isPercentage ? slider.value + '%' : slider.value;
-}
+function calculateEstimatedRevenue() {
+    const totalUsers = parseInt(document.getElementById('total-users').value, 10);
+    const percentYearly = parseFloat(document.getElementById('percent-yearly').value) / 100;
+    const percentMonthly = 1 - percentYearly;
+    const generalConversionRate = parseFloat(document.getElementById('conversion-rate').value) / 100;
 
-// Function to calculate and update the LTV
-function calculateAndUpdateLTV() {
-    // Extract values from sliders and inputs
     const monthlyPrice = parseFloat(document.getElementById('monthly-price').value);
+    const monthlyDiscount = parseFloat(document.getElementById('monthly-intro-discount').value) / 100;
+    const monthlyDiscountPeriod = parseInt(document.getElementById('monthly-intro-period').value, 10);
+
     const yearlyPrice = parseFloat(document.getElementById('yearly-price').value);
-    const conversionRate = parseFloat(document.getElementById('conversion-rate').value) / 100;
-    const ltv = parseFloat(document.getElementById('ltv').value);
-    const introPriceMonthly = parseFloat(document.getElementById('intro-price-monthly').value);
-    const introPeriodMonthly = parseInt(document.getElementById('intro-period-monthly').value, 10);
-    const introPriceYearly = parseFloat(document.getElementById('intro-price-yearly').value);
-    const introPeriodYearly = parseInt(document.getElementById('intro-period-yearly').value, 10);
+    const yearlyDiscount = parseFloat(document.getElementById('yearly-intro-discount').value) / 100;
+    const yearlyDiscountPeriod = parseInt(document.getElementById('yearly-intro-period').value, 10);
 
-    // Calculate the effective monthly and yearly prices considering introductory prices
-    let effectiveMonthlyPrice = introPriceMonthly && introPeriodMonthly ? introPriceMonthly : monthlyPrice;
-    let effectiveYearlyPrice = introPriceYearly && introPeriodYearly ? introPriceYearly : yearlyPrice;
+    let monthlyRevenue = monthlyPrice * generalConversionRate * totalUsers * percentMonthly;
+    let yearlyRevenue = yearlyPrice * generalConversionRate * totalUsers * percentYearly;
 
-    // Calculate LTV
-    let calculatedLTV = (effectiveMonthlyPrice * conversionRate * ltv) + (effectiveYearlyPrice * conversionRate * ltv);
+    // Apply discounts
+    monthlyRevenue *= (1 - monthlyDiscount) * Math.max(1, monthlyDiscountPeriod);
+    yearlyRevenue *= (1 - yearlyDiscount) * Math.max(1, yearlyDiscountPeriod);
 
-    // Update the result display
-    document.getElementById('result').textContent = calculatedLTV.toFixed(2);
+    let estimatedRevenue = monthlyRevenue + yearlyRevenue;
+
+    document.getElementById('result').textContent = estimatedRevenue.toFixed(2);
+    document.getElementById('percent-yearly-value').textContent = `${(percentYearly * 100).toFixed(0)}%`;
+
+    // Breakdown display
+    let breakdownText = `
+        <strong>Total Users:</strong> ${totalUsers}<br>
+        <strong>% Yearly:</strong> ${(percentYearly * 100).toFixed(2)}%<br>
+        <strong>General Conversion Rate:</strong> ${(generalConversionRate * 100).toFixed(2)}%<br>
+        <strong>Monthly Revenue:</strong> ${monthlyRevenue.toFixed(2)}<br>
+        <strong>Yearly Revenue:</strong> ${yearlyRevenue.toFixed(2)}<br>
+        <strong>Estimated Total Revenue:</strong> ${estimatedRevenue.toFixed(2)}
+    `;
+    document.getElementById('breakdown').innerHTML = breakdownText;
 }
 
-// Event listeners for sliders
-document.querySelectorAll('.slider').forEach(slider => {
-    slider.addEventListener('input', () => {
-        updateSliderValue(slider.id, slider.id + '-value', slider.id === 'conversion-rate');
-        calculateAndUpdateLTV();
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('input').forEach(input => {
+        input.addEventListener('input', calculateEstimatedRevenue);
     });
-});
 
-// Initial calculation and display update
-window.onload = () => {
-    calculateAndUpdateLTV();
-    document.querySelectorAll('.slider').forEach(slider => {
-        updateSliderValue(slider.id, slider.id + '-value', slider.id === 'conversion-rate');
-    });
-};
+    calculateEstimatedRevenue();
+});
